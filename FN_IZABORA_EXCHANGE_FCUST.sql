8c3525b0aaad8f6af0e8a362c507522f9cc655e1
i-iza <144711536+i-iza@users.noreply.github.com>
2023-11-02 15:52:09 +0100

    Add files via upload

---
+++ FN_IZABORA_EXCHANGE_FCUST.sql
@@ -0,0 +1,44 @@
+create or replace FUNCTION FN_IZABORA_EXCHANGE_FCUST (
+                    p_ccy_from IN VARCHAR2,
+                    p_ccy_to IN VARCHAR2,
+                    p_amount IN NUMBER,
+                    p_branch_code IN VARCHAR2
+)RETURN NUMBER AS
+    l_result_amount     NUMBER(22,3);
+    l_mid_rate          NUMBER(24,12);
+    l_digits_after      NUMBER(20);
+    l_last_change       DATE;
+
+BEGIN
+  --First we find the mid rate between the specified currencies using the function we created recently
+    l_mid_rate := fn_IZABORA_MID_RATE_FCUST(p_ccy_from, p_ccy_to, p_branch_code);
+    --To calculate the amount in a different currency, we multiply the amount with the mid rate
+    l_result_amount := p_amount * l_mid_rate;
+    
+    --Search for the last updated version
+    SELECT MAX(maker_dt_stamp) INTO l_last_change
+    FROM CYTM_CCY_DEFN_MASTER
+    WHERE ccy_code = p_ccy_to;
+    
+    --In case of multiple records of the same date, we need to choose the one with most digits after the decimal point, in order to be more precise
+    --If precision does not matter, then we can change the query by using the keyword DISTINCT
+    SELECT MAX(ccy_decimals) INTO l_digits_after
+    FROM CYTM_CCY_DEFN_MASTER
+    WHERE ccy_code = p_ccy_to
+    AND maker_dt_stamp = l_last_change;    
+    
+    --Check if the currency has the number of decimal digits specified
+    --If yes, we round up the amount
+    --If not, we just return the calculated amount
+    IF l_digits_after IS NOT NULL THEN
+      l_result_amount := ROUND(l_result_amount, l_digits_after);
+      RETURN l_result_amount;
+    END IF;
+    RETURN l_result_amount;
+    
+EXCEPTION
+    WHEN OTHERS THEN 
+        DBMS_OUTPUT.PUT_LINE(SQLERRM);
+        DBMS_OUTPUT.PUT_LINE(dbms_utility.format_error_backtrace);
+        RAISE;
+END FN_IZABORA_EXCHANGE_FCUST;
---
+++ FN_IZABORA_ISHOLIDAY_FCUST.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE FUNCTION FN_IZABORA_ISHOLIDAY_FCUST(
+    p_dateParam IN DATE,
+    p_nParam IN NUMBER,
+    p_HorW OUT VARCHAR2,
+    p_error_message OUT VARCHAR2
+)RETURN BOOLEAN IS
+    l_myDate DATE;
+    l_holidayList STTM_LCL_HOLIDAY.holiday_list%TYPE;
+    l_month_number NUMBER;
+    l_year_number NUMBER;
+    l_day_number NUMBER;
+    l_holiday_status VARCHAR2(1); 
+BEGIN
+  -- Initialize the date based on whether n is negative or positive
+   IF p_nParam < 0 THEN
+      l_myDate := p_dateParam - ABS(p_nParam);
+      p_HorW := 'before';
+   ELSE
+      l_myDate := p_dateParam + p_nParam;
+      p_HorW := 'from';
+   END IF;
+   
+   --The three lines of code below store the day, month and year of the date to be queried
+   --such that we are able to search for it in the table
+    l_month_number := EXTRACT(MONTH FROM l_myDate);
+    l_year_number := EXTRACT(YEAR FROM l_myDate);
+    l_day_number := EXTRACT(DAY FROM l_myDate);
+
+   -- Check if the date is a holiday (day off)(assuming holidays on weekends)
+   IF TO_CHAR(l_myDate, 'D') = '1' OR TO_CHAR(l_myDate, 'D') = '7' THEN -- extract the day of the week as a number
+      p_error_message := 'Date is a weekend holiday.';
+      RETURN TRUE;
+   END IF;
+   
+    --Query the table to store the list of characters (h and w), in order to check later if 
+    --the l_day_number-th character is h
+    SELECT holiday_list
+    INTO l_holidayList
+    FROM STTM_LCL_HOLIDAY
+    WHERE year = l_year_number AND month = l_month_number;
+    
+    l_holiday_status := SUBSTR(l_holidayList, l_day_number , 1);
+    IF l_holiday_status = 'H' THEN
+         RETURN TRUE;
+    ELSIF l_holiday_status = 'W' THEN
+        RETURN FALSE;
+    END IF;
+    
+EXCEPTION
+   WHEN NO_DATA_FOUND THEN --If the date is not at all in the table, 
+                           --eg. if the year entered is earlier or later than the ones in the table
+        p_error_message := 'Date not found in the holiday table.';
+        RETURN FALSE;
+   WHEN OTHERS THEN
+      p_error_message := SQLERRM;
+      DBMS_OUTPUT.PUT_LINE(SQLERRM);
+      DBMS_OUTPUT.PUT_LINE(dbms_utility.format_error_backtrace);
+      RAISE;
+END FN_IZABORA_ISHOLIDAY_FCUST;
