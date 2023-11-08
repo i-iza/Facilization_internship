fe35ffb573a4334b14d75a3eb0c9c80fef32dc3f
i-iza <144711536+i-iza@users.noreply.github.com>
2023-11-08 15:58:52 +0100

    Delete Exercise_1/IZABORA_XML_TAB_SEQ.sql

--- Exercise_1/IZABORA_XML_TAB_SEQ.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  File created - Monday-November-06-2023   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Sequence IZABORA_XML_TAB_SEQ
---------------------------------------------------------
-
-   CREATE SEQUENCE  "FC143"."IZABORA_XML_TAB_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
