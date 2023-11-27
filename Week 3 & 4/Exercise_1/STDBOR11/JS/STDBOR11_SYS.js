599e4c54197d5a0f1ac64ca3bb5f79ca79340073
i-iza <144711536+i-iza@users.noreply.github.com>
2023-11-27 03:25:46 -0800

    Add files via upload

---
+++ Week 3 & 4/Exercise_1/STDBOR11/JS/STDBOR11_SYS.js
@@ -0,0 +1,187 @@
+/***************************************************************************************************************************
+**  This source is part of the FLEXCUBE Software Product. 
+**  Copyright (c) 2008 ,2023, Oracle and/or its affiliates.
+**  All rights reserved.
+**  
+**  No part of this work may be reproduced, stored in a retrieval system, 
+**  adopted or transmitted in any form or by any means, electronic, mechanical, photographic, 
+**  graphic, optic recording or otherwise, translated in any language or computer language, 
+**  without the prior written permission of Oracle and/or its affiliates.
+**  
+**  Oracle Financial Services Software Limited.
+**  Oracle Park, Off Western Express Highway,
+**  Goregaon (East),
+**  Mumbai - 400 063,
+**  India.
+**  
+**  Written by         : 
+**  Date of creation   : 
+**  File Name          : STDBOR11_SYS.js
+**  Purpose            : 
+**  Called From        : 
+**  
+**  CHANGE LOG
+**  Last Modified By   : 
+**  Last modified on   : 
+**  Full Version       : 
+**  Reason             : 
+****************************************************************************************************************************/
+
+//***** Code for criteria Search *****
+var criteriaSearch  = 'N';
+//----------------------------------------------------------------------------------------------------------------------
+//***** FCJ XML FOR THE SCREEN *****
+//----------------------------------------------------------------------------------------------------------------------
+var fieldNameArray = {"BLK_GROUP":"GROUP_CODE~DESCRIPT~EXPOSURE_CATEGORY~GROUP_STATUS~GROUP_TYPE~SINCE~MAKER~MAKERSTAMP~CHECKER~CHECKERSTAMP~MODNO~TXNSTAT~AUTHSTAT~ONCEAUTH"};
+
+var multipleEntryPageSize = {};
+
+var multipleEntrySVBlocks = "";
+
+var tabMEBlks = {};
+
+var msgxml=""; 
+msgxml += '    <FLD>'; 
+msgxml += '      <FN PARENT="" RELATION_TYPE="1" TYPE="BLK_GROUP">GROUP_CODE~DESCRIPT~EXPOSURE_CATEGORY~GROUP_STATUS~GROUP_TYPE~SINCE~MAKER~MAKERSTAMP~CHECKER~CHECKERSTAMP~MODNO~TXNSTAT~AUTHSTAT~ONCEAUTH</FN>'; 
+msgxml += '    </FLD>'; 
+
+var strScreenName = "CVS_GROUP_CODE_MAINTENANCE";
+var qryReqd = "Y";
+var txnBranchFld = "" ;
+var originSystem = "";
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//***** FCJ XML FOR SUMMARY SCREEN *****
+//----------------------------------------------------------------------------------------------------------------------
+var msgxml_sum=""; 
+msgxml_sum += '    <FLD>'; 
+msgxml_sum += '      <FN PARENT="" RELATION_TYPE="1" TYPE="BLK_GROUP">AUTHSTAT~TXNSTAT~DESCRIPT~EXPOSURE_CATEGORY~GROUP_CODE~GROUP_STATUS~GROUP_TYPE~SINCE</FN>'; 
+msgxml_sum += '    </FLD>'; 
+
+var detailFuncId = "STDBOR11";
+var defaultWhereClause = "";
+var defaultOrderByClause ="";
+var multiBrnWhereClause ="";
+var g_SummaryType ="S";
+var g_SummaryBtnCount =0;
+var g_SummaryBlock ="BLK_GROUP";
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR DATABINDING *****
+//----------------------------------------------------------------------------------------------------------------------
+ var relationArray = {"BLK_GROUP" : ""}; 
+
+ var dataSrcLocationArray = new Array("BLK_GROUP"); 
+ // Array of all Data Sources used in the screen 
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR QUERY MODE *****
+//----------------------------------------------------------------------------------------------------------------------
+var detailRequired = true ;
+var intCurrentQueryResultIndex = 0;
+var intCurrentQueryRecordCount = 0;
+
+var queryFields = new Array();    //Values should be set inside STDBOR11.js, in "BlockName__FieldName" format
+var pkFields    = new Array();    //Values should be set inside STDBOR11.js, in "BlockName__FieldName" format
+queryFields[0] = "BLK_GROUP__GROUP_CODE";
+pkFields[0] = "BLK_GROUP__GROUP_CODE";
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR AMENDABLE/SUBSYSTEM Fields *****
+//----------------------------------------------------------------------------------------------------------------------
+//***** Fields Amendable while Modification *****
+var modifyAmendArr = {"BLK_GROUP":["DESCRIPT","EXPOSURE_CATEGORY","GROUP_CODE","GROUP_STATUS","GROUP_TYPE","SINCEI"]};
+var closeAmendArr = new Array(); 
+var reopenAmendArr = new Array(); 
+var reverseAmendArr = new Array(); 
+//***** Fields Amendable while Delete *****
+var deleteAmendArr = {"BLK_GROUP":["DESCRIPT","EXPOSURE_CATEGORY","GROUP_CODE","GROUP_STATUS","GROUP_TYPE","SINCEI"]};
+var rolloverAmendArr = new Array(); 
+var confirmAmendArr = new Array(); 
+var liquidateAmendArr = new Array(); 
+//***** Fields Amendable while Query *****
+var queryAmendArr = {"BLK_GROUP":["DESCRIPT","EXPOSURE_CATEGORY","GROUP_CODE","GROUP_STATUS","GROUP_TYPE","SINCEI"]};
+var authorizeAmendArr = new Array(); 
+//----------------------------------------------------------------------------------------------------------------------
+
+var subsysArr    = new Array(); 
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//***** CODE FOR LOVs *****
+//----------------------------------------------------------------------------------------------------------------------
+var lovInfoFlds = {"BLK_GROUP__EXPOSURE_CATEGORY__LOV_EXPOSURE":["BLK_GROUP__EXPOSURE_CATEGORY~","","N",""]};
+var offlineLovInfoFlds = {};
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR TABS *****
+//----------------------------------------------------------------------------------------------------------------------
+var strHeaderTabId = 'TAB_HEADER';
+var strFooterTabId = 'TAB_FOOTER';
+var strCurrentTabId = 'TAB_MAIN';
+//--------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR MULTIPLE ENTRY BLOCKS *****
+//----------------------------------------------------------------------------------------------------------------------
+var multipleEntryIDs = new Array();
+var multipleEntryArray = new Array();
+var multipleEntryCells = new Array();
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR MULTIPLE ENTRY VIEW SINGLE ENTRY BLOCKS *****
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR ATTACHED CALLFORMS *****
+ //----------------------------------------------------------------------------------------------------------------------
+
+ var CallFormArray= new Array(); 
+
+ var CallFormRelat=new Array(); 
+
+ var CallRelatType= new Array(); 
+
+
+ var ArrFuncOrigin=new Array();
+ var ArrPrntFunc=new Array();
+ var ArrPrntOrigin=new Array();
+ var ArrRoutingType=new Array();
+
+
+ // Code for Loading Cluster/Custom js File Starts
+ var ArrClusterModified=new Array();
+ var ArrCustomModified=new Array();
+ // Code for Loading Cluster/Custom js File ends
+
+ArrFuncOrigin["STDBOR11"]="CUSTOM";
+ArrPrntFunc["STDBOR11"]="";
+ArrPrntOrigin["STDBOR11"]="";
+ArrRoutingType["STDBOR11"]="X";
+
+
+ // Code for Loading Cluster/Custom js File Starts
+ArrClusterModified["STDBOR11"]="N";
+ArrCustomModified["STDBOR11"]="Y";
+
+ // Code for Loading Cluster/Custom js File ends
+
+
+ /* Code For OBIEE functionalities */ 
+var obScrArgName  = new Array(); 
+var obScrArgSource  = new Array(); 
+//***** CODE FOR SCREEN ARGS *****
+//----------------------------------------------------------------------------------------------------------------------
+var scrArgName = {};
+var scrArgSource = {};
+var scrArgVals = {};
+var scrArgDest = {};
+//***** CODE FOR SUB-SYSTEM DEPENDENT  FIELDS   *****
+//----------------------------------------------------------------------------------------------------------------------
+var dpndntOnFlds = {};
+var dpndntOnSrvs = {};
+//***** CODE FOR TAB DEPENDENT  FIELDS   *****
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR CALLFORM TABS *****
+//----------------------------------------------------------------------------------------------------------------------
+var callformTabArray = new Array(); 
+//***** CODE FOR ACTION STAGE DETAILS *****
+//----------------------------------------------------------------------------------------------------------------------
+var actStageArry = {"QUERY":"2","NEW":"2","MODIFY":"2","AUTHORIZE":"1","DELETE":"1","CLOSE":"1","REOPEN":"1","REVERSE":"1","ROLLOVER":"1","CONFIRM":"1","LIQUIDATE":"1","SUMMARYQUERY":"2"};
+//***** CODE FOR IMAGE FLDSET *****
+//----------------------------------------------------------------------------------------------------------------------
