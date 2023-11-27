074a8f0d95c9d88aceb7c3e2e879b42219b8b4e7
i-iza <144711536+i-iza@users.noreply.github.com>
2023-11-27 11:04:25 +0100

    Delete Exercise 1 SpringBoot directory

--- Exercise 1 SpringBoot/src/main/java/com/BORA/Customer_info/repository/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.BORA.Customer_info.repository;
-
-import com.BORA.Customer_info.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepo extends JpaRepository<Account, Long> {
-}
